7513873f99dd3b513d210057d9fce5614ec7cdc0
1E10 Laptop <sample@bths.edu>
2022-10-07 09:49:02 -0400

    Hi

---
+++ src/LabRunner.java
@@ -0,0 +1,13 @@
+public class LabRunner {
+    public static void main(String[] args) {
+        String str1 = "Friday";
+        String str2 = new String("October");
+        String s = "7th!";
+        String str3 = new String(s);
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        // Hi
+    }
+}
